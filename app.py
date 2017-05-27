a8a1f8b928fe4229262c7867f796fa20baf6bd6e
Eduardo Leoni <eduardoleoni@otlook.com>
2017-05-28 09:56:57 +1200

    Have it temporarily write to file whilst it queue isn't implemented

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import base64
 import speech_recognition as sr
 
-print('ready');
-
 r = sr.Recognizer()
-while True:
-    with sr.Microphone() as source:
+
+with sr.Microphone() as source:
+    #while True:
+        r.adjust_for_ambient_noise(source)
+        r.energy_threshold = 1500
         audio = r.listen(source)
-        print ('Hey I heard something')
+        b64 = base64.b64encode(audio.get_wav_data())
+
+        file = open("testfile.txt","wb")
+ 
+        file.write(b64) 
+        file.close()
