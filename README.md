75aba632a40d4f9b9ffe4e91e5bfcb05e3e2bfe9
Eduardo Leoni <eduardoleoni@otlook.com>
2017-05-28 18:08:44 +1200

    Redefine approach so that it also converts to string before converting to b64, adding arguments so that the location of the auditory-cortex can be defined

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Cochlea
 
 The cochlea /ˈkɒk.liə/ (Ancient Greek: κοχλίας, kōhlias, meaning spiral or snail shell) is the auditory portion of the inner ear. Matilda's cochlea is a Python app that is constantly listening to the environment looking for commands.
+
+
+## Installation
+```
+pip install -r requirements.txt
+python app.py [<url to auditory-cortex>](https://github.com/TheMatildaProject/auditory-cortex)
+```
+
+So far I haven't been able to bind the host's microphone to the docker container, therefore can't offer this as a Docker image, if anyone has any clues on how to do this, I'd be most grateful if you sent a PR.
--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 import base64
+import json
+import requests
 import speech_recognition as sr
+import sys
 
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    #while True:
+    while True:
         r.adjust_for_ambient_noise(source)
         r.energy_threshold = 1500
         audio = r.listen(source)
         b64 = base64.b64encode(audio.get_wav_data())
-
-        file = open("testfile.txt","wb")
- 
-        file.write(b64) 
-        file.close()
+        
+        payload = {"audio": "{0}".format(b64.decode())} # byte to string as json is prep
+        
+        rq = requests.post(sys.argv[1], json=payload);
+        
+        print (rq);
